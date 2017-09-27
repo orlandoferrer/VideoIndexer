829281c117654c2780eeca23d5d3ef57aabff05a
Orlando Ferrer <orlando.ferrer@gmail.com>
2017-09-27 18:48:20 -0400

    Hardcoded test values
    testing with hardcoded values

--- indexer.py
+++ indexer.py
@@ -1,4 +1,5 @@
 import json
+from subprocess import call
 magicbucket = json.load(open('magicbucket.json'))
 
 ##Only keep video files
@@ -7,4 +8,8 @@ magicbucket = json.load(open('magicbucket.json'))
 #for item in magicbucket["files"]:
 #    print (item["fileName"])
 
-#///////
+#///////hardcoded test values
+videolist = ["Contact.mp4", "Dai Vernon CBS Interview 1987.mp4", "Joshua Jay - Talk About Tricks Vol 1.mp4", "Lee Asher - Cooking with Lee Asher Well Done.avi", "The School of Cool by Greg Wilson.mov"]
+for i in videolist:
+    #print(i)
+    call(["vcs", i, "-U0 -n 20 -c 5 -H 200 -a 300/200 -o", i + ".jpg"])
